961514a7ea488af7631b8fc1072dd38be90f42f6
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-03-01 16:06:10 +0530

    error fix

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,16 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Slider = ({ start }) => {
     return (
-        <Carousel>
-            {start.map((item) => {
+        <Carousel fade>
+            {start.map((item) => (
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={item}
                         alt="First slide"
                     />
-                </Carousel.Item>;
-            })}
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 };
