36d77979930d6433e0cbc8602a4b3a603ea53d01
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-03-01 11:09:52 +0530

    er

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import Slider from "./components/Slider";
-import banner from "./data/data.json";
-
+import data from "./data/data.json";
 function App() {
     return (
         <BrowserRouter>
             <PreNavbar />
             <Navbar />
-            <Slider start={banner.start} />{" "}
+            <Slider start={data.banner.start} />
         </BrowserRouter>
     );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,18 @@
-import { Carousel } from "bootstrap";
-
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
-const Slider = ({ start }) => {
+const Slider = ({ item }) => {
     return (
-        <Carousel fade>
-            {start.map((item) => (
+        <Carousel>
+            {start.map((item) => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={item}
+                        src="https://i02.appmifile.com/173_operator_in/09/06/2021/fde866737ea0357305c2d1c852541460.gif"
                         alt="First slide"
                     />
-                </Carousel.Item>
-            ))}
+                </Carousel.Item>;
+            })}
         </Carousel>
     );
 };
