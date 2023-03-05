936eb5632c546f1daedbec81ed412ffb29575f74
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-03-05 11:45:40 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -11,37 +11,26 @@ import data from "./data/data.json";
 import ProductReview from "./components/ProductReview";
 import Video from "./components/Video.jsx";
 import Banner from "./components/Banner.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <PreNavbar />
-
             <Navbar />
-
             <Slider start={data.banner.start} />
-
             <Offers offer={data.offer} />
-
             <Heading text="STAR PRODUCTS" />
-
             <StarProduct starProduct={data.starProduct} />
-
             <Heading text="HOT ACCESSORIES" />
-
             <HotAccessoriesMenu />
-
             <Heading text="PRODUCT REVIEWS" />
-
             <ProductReview productReviews={data.productReviews} />
-
             <Heading text="VIDEOS" />
-
             <Video videos={data.videos} />
-
             <Heading text="IN THE PRESS" />
-
-            <Banner banner={data.banner} />
+            <Banner banner={data.banner} />\
+            <Footer />
         </BrowserRouter>
     );
 }
